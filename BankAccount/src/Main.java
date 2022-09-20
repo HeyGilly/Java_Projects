ee389d1757cb6e079abbc52e18cd78fb7e29f9d6
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-09-19 22:06:26 -0500

    the user will input his username and account number then showcase the results.

--- BankAccount/src/Main.java
+++ BankAccount/src/Main.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        //-- Gather User's input
+        Scanner input = new Scanner(System.in);
+        //-- Start of application
+        System.out.println("***********************");
+        //-- Asking the user for their Username & 4 digit pin
+        System.out.println("Enter your Username:");
+        String username = input.nextLine();
+        System.out.println("Enter your Account Number:");
+        String accountNumber = input.nextLine();
+
+        //-- Once approved, Run the Bank Application
+
+        BankAccount AccountUser = new BankAccount(username, accountNumber);
+         System.out.println(AccountUser.Username);
+         System.out.println(AccountUser.userAccountNumber);
+
+
+    }
 }
