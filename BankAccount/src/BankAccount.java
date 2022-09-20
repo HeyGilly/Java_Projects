597222f509b117cc080296563c6d75bfbf27c75f
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-09-19 22:06:59 -0500

    Created variables and constructors to showcase the users input

--- BankAccount/src/BankAccount.java
+++ BankAccount/src/BankAccount.java
@@ -1,2 +1,14 @@
 public class BankAccount {
+
+    //-- Creation of Instance Variables
+    double balance;
+    double previousTransaction;
+    String Username;
+    String userAccountNumber;
+
+    //-- Create a constructor that will make it accessible for the username and Pin.
+    public BankAccount(String Username, String userAccountNumber) {
+        this.Username = Username;
+        this.userAccountNumber = userAccountNumber;
+    }
 }
