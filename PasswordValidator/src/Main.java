48c57c2b98f76d9e5bb081f44fdded1a097cb9d8
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-09-21 16:46:21 -0500

    imported scanner to take in an input, a do-while loop that will check to see if the users new password is valid. if not valid will run again with an error message

--- PasswordValidator/src/Main.java
+++ PasswordValidator/src/Main.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         //-- Creating an instance of Password Validator
         PasswordValidator userPassword = new PasswordValidator();
         //-- Password Rules
         PasswordValidator.printPasswordRules();
+        //-- if valid true or false
+        boolean valid;
+
+        do {
+            //-- ask the user for their password.
+            System.out.println("Enter your new password:");
+            //-- capturing the users input password
+            var proposedPassword = scanner.nextLine();
+            valid = PasswordValidator.changePassword(proposedPassword);
+        }while(!valid);
+
+        //-- Will run once the password is valid.
+        System.out.println("The proposed password is valid!");
+        //-- Closing the scanner
+        scanner.close();
 
 
     }
