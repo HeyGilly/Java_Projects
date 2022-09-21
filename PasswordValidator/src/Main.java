2e9d295409bc95c6cd12fb32402cf27a7cdf7c5e
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-09-21 15:42:31 -0500

    showcasing the rules of the password

--- PasswordValidator/src/Main.java
+++ PasswordValidator/src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
+        //-- Creating an instance of Password Validator
+        PasswordValidator userPassword = new PasswordValidator();
+        //-- Password Rules
+        PasswordValidator.printPasswordRules();
+
 
     }
 
--- PasswordValidator/src/PasswordValidator.java
+++ PasswordValidator/src/PasswordValidator.java
@@ -1,4 +1,22 @@
 public class PasswordValidator {
 
 
+
+    public static void printPasswordRules(){
+        System.out.println();
+        System.out.println("***********************");
+        System.out.println(" Your new password must meet the following requirements:");
+        System.out.println("   * at least 8 characters long");
+        System.out.println("   * contain an uppercase letter");
+        System.out.println("   * contain a special character");
+        System.out.println("   * not contain the username");
+        System.out.println("   * not the same as the old password");
+        System.out.println("***********************");
+        System.out.println();
+    }
+
+
+
+
+
 }
