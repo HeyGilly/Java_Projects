c0fe030c13af87c2155af0f456b58446b89b2bd9
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-09-23 13:41:09 -0500

    Initial Commit

---
+++ EmailApplication/src/EmailApp/Main.java
@@ -0,0 +1,10 @@
+package EmailApp;
+
+//----- Scenario: I play an IT Supper Administrator, and I am in charge with the task of creating email account for new hires.
+//--- [ ]   Generate an email with the following syntax: firstname.lastname@department.company.com
+//--- [ ] Determine the departments (Sales, development, accounting), if non leave blank
+//--- [ ] Have set methods to change the password, set the mailbox capacity, and define an alternate email address
+//--- [ ] Have get methods to display the name and mailbox capacity
+
+public class Main {
+}
