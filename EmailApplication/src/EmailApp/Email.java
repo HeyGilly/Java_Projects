688a44611f33ff73eaa1fd74898e6cffa2b75ac1
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-09-23 16:57:19 -0500

    Created a method for the email created and showing of a department

--- EmailApplication/src/EmailApp/Email.java
+++ EmailApplication/src/EmailApp/Email.java
@@ -1,4 +1,61 @@
 package EmailApp;
 
+import java.util.Scanner;
+
 public class Email {
+    //-- Properties
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    //-- Constructor to receive the first name and last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+
+        // Call a method asking for the department - return the department
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+    }
+
+    //-- Ask for department
+        private String setDepartment(){
+            System.out.println();
+            System.out.println("****************************");
+            System.out.println("DEPARTMENT CODE");
+            System.out.println(" 1) for Sales\n 2) for Development\n 3) for Accounting\n 0) for none");
+            System.out.println("Enter the department code:");
+            Scanner in = new Scanner(System.in);
+            int depChoice = in.nextInt();
+            String userDepartment = "";
+            //-- Switch Conditional
+            if(depChoice == 1){
+                userDepartment = "Sales";
+            }else if (depChoice == 2){
+                userDepartment = "Development";
+            }else if (depChoice == 3){
+                userDepartment = "Accounting";
+            } else if (depChoice == 0){
+                userDepartment = "";
+            }else{
+                System.out.println();
+                System.out.println("Please insert the proper department!!!");
+                in.close();
+                setDepartment();
+            }
+            return userDepartment;
+        }
+
+
+    //-- Generate Random Password
+
+    //--- Set the mailbox capacity
+
+    //-- set the alternate email
+
+    //-- Change the password
 }
