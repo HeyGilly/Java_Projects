4cd85ad50f26ef50cfcc16ee9bd258e0140bede0
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-09-23 16:55:58 -0500

    Created an introduction that takes a user first and last name

--- EmailApplication/src/EmailApp/Main.java
+++ EmailApplication/src/EmailApp/Main.java
@@ -6,5 +6,25 @@ package EmailApp;
 //--- [ ] Have set methods to change the password, set the mailbox capacity, and define an alternate email address
 //--- [ ] Have get methods to display the name and mailbox capacity
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //-- Build an introduction that will ask the user to input first name and last name.
+        System.out.println();
+        System.out.println("****************************");
+        System.out.println("Welcome to High School High!");
+        System.out.println("New hires will have to provide first name, last name and what department you will are assigned to.");
+        System.out.println("First Name:");
+        String firstName = sc.nextLine();
+        System.out.println("Last Name:");
+        String lastName = sc.nextLine();
+
+
+
+        Email userEmail = new Email(firstName,lastName);
+
+    }
 }
