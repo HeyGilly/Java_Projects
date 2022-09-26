5bdb5f76e3f7f07db94db83e203dc6d3007dadca
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-09-26 14:51:35 -0500

    Build variables that are needed.

--- StudentDatabaseApplication/src/StudentDB/Student.java
+++ StudentDatabaseApplication/src/StudentDB/Student.java
@@ -1,4 +1,23 @@
 package StudentDB;
 
 public class Student {
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String courses;
+    private String tuitionBalance;
+    private int costOfCourse = 600;
+
+    //-- Constructor: Prompt the user to enter the Students name and current year.
+
+    //-- Generate an ID;
+
+    //-- Enroll in courses
+
+    //-- View Balance
+
+    //-- Pay tuition
+
+    //show status
 }
