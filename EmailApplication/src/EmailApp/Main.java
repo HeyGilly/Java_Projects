c37ec0655658dac9f68f55f70945278ac4f84759
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-09-26 14:25:14 -0500

    call the method to showcase the correct info

--- EmailApplication/src/EmailApp/Main.java
+++ EmailApplication/src/EmailApp/Main.java
@@ -24,7 +24,6 @@ public class Main {
 
         Email userEmail = new Email(firstName,lastName);
 
-        userEmail.setAlternateEmail("alternativeEmail@gmail.com");
-
+        System.out.println(userEmail.showInfo());
     }
 }
