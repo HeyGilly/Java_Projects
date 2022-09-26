4575d885158b22e4bed2765193e2d0693d6b7d58
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-09-26 16:43:54 -0500

    Created a userID for all students that enroll

--- StudentDatabaseApplication/src/StudentDB/Student.java
+++ StudentDatabaseApplication/src/StudentDB/Student.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class Student {
     private String firstName;
     private String lastName;
+    private int userInput;
     private int gradeYear;
-    private int studentID;
+    private String studentID;
     private String courses;
     private String tuitionBalance;
-    private int costOfCourse = 600;
+    private static int costOfCourse = 600;
+    private static int id = 5000;
 
     //-- Constructor: Prompt the user to enter the Students name and current year.
     public Student(){
@@ -26,25 +28,31 @@ public class Student {
                 3 - Junior
                 4 - Senior\040
                 """);
-        this.gradeYear = sc.nextInt();
+        this.userInput = sc.nextInt();
         // once the user input 1-4, it would be put in the correct grade
-        switch (gradeYear) {
+        switch (userInput) {
             case 1 -> this.gradeYear = 9;
             case 2 -> this.gradeYear = 10;
             case 3 -> this.gradeYear = 11;
             case 4 -> this.gradeYear = 12;
         }
 
+        // Set the student ID
+        setStudentID();
 
         System.out.println("********************");
         System.out.println();
-        System.out.println(firstName +" "+ lastName +" "+ gradeYear+"th grade");
+        System.out.println(firstName +" "+ lastName +" "+ gradeYear+"th grade with an ID of "+studentID);
 
     }
 
 
     //-- Generate an ID;
-
+        private void setStudentID(){
+            id++;
+            // GradeLevel + ID
+            this.studentID = userInput + "" + id;
+        }
     //-- Enroll in courses
 
     //-- View Balance
