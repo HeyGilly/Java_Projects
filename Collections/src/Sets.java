f31c6b02e063dd49b126250a9345d530630403bf
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-11-12 16:29:26 -0600

    rename main

--- Collections/src/Sets.java
+++ Collections/src/Sets.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Sets {
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
 
         Set fruits = new HashSet();
         fruits.add("apple");
